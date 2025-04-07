1d788ce3a6b16a0cbd49f34c549e55ae0a2f529f
Minh Ngoc Doan <minhngocdoan3112@gmail.com>
2025-04-07 16:40:41 +0700

    Setting up ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'path';
+import { configValidationSchema } from './configs/config.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`,
+      validationSchema: configValidationSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
