f7fbf0fd51eb97fc4783ba392dad418d946f0f81
Minh Ngoc Doan <minhngocdoan3112@gmail.com>
2025-04-08 13:57:27 +0700

    Update database configuration in TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,33 +4,23 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as path from 'path';
 import { configValidationSchema } from './configs/config.schema';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from './modules/jwt/jwt.module';
+import typeorm from './databases/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: path.join(
-        __dirname,
-        `../src/configs/.env-${process.env.NODE_ENV || 'dev'}`,
-      ),
-      //`.env.${process.env.NODE_ENV || 'dev'}`
-      validationSchema: configValidationSchema,
+      load: [typeorm],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        database: configService.get('POSTGRES_DATABASE'),
-        password: configService.get('POSTGRES_PASSWORD'),
-      }),
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm') as TypeOrmModuleOptions,
     }),
     UserModule,
     AuthModule,
